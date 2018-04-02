a323b27d39f022717b5116fc1b63f434a505eeaa
Nathan Reed <nathreed@gmail.com>
2018-04-02 12:16:00 -0400

    More tests and code improvements - Removed the convention of returning a coordinate with Double.NaN for when things don't intersect - Double.NaN is not comparable to anything so this messes with the equals() method - Replaced this convention with Java 8 Optionals - Added more unit tests for Coordinates2D and fixed a few small bugs they brought to light - Added IntelliJ out directory to gitignore

--- src/main/java/Coordinates2D.java
+++ src/main/java/Coordinates2D.java
@@ -1,5 +1,4 @@
-import com.lemmingapex.trilateration.NonLinearLeastSquaresSolver;
-
+import java.util.Optional;
 
 /*
 This class describes a 2D set of coordinates and provides helpful methods for dealing with their geometry
@@ -35,10 +34,14 @@ public class Coordinates2D {
      * @return The distance between the centers of the circles given by the two Coordinates2D objects.
      *
      */
-    public double distance(Coordinates2D b) {
+    public Optional<Double> distance(Coordinates2D b) {
+        if(b == null) {
+            //Can't do distance to a nonexistent point
+            return Optional.empty();
+        }
         //Distance formula
         //sqrt(x2-x1)^2 - (y2-y1)^2
-        return Math.sqrt(Math.pow(b.getX() - this.x, 2) + Math.pow(b.getY() - this.y, 2));
+        return Optional.of(Math.sqrt(Math.pow(b.getX() - this.x, 2) + Math.pow(b.getY() - this.y, 2)));
     }
 
     /**
@@ -49,43 +52,54 @@ public class Coordinates2D {
      * @return A Coordinates2D object. If there was no intersection, the x and y coordinates will be Double.NaN
      *
      */
-    public Coordinates2D circleIntersection(Coordinates2D b) {
+    public Optional<Coordinates2D> circleIntersection(Coordinates2D b) {
+        if(b == null) {
+            //Can't compute intersection with a null circle. And we can't make b an optional parameter (optional parameters not allowed)
+            return Optional.empty();
+        }
         //Circle intersection calculated using https://math.stackexchange.com/a/1367732
         //Code adapted from https://gist.github.com/jupdike/bfe5eb23d1c395d8a0a1a4ddd94882ac
         double aRadius = (this.xErr + this.yErr) / 2;
         double bRadius = (b.getXErr() + b.getYErr()) / 2;
 
         //Analogous to variable R in gist code
-        double centerDistance = distance(b);
+        Optional<Double> optCenterDistance = distance(b);
 
-        if(!(Math.abs(aRadius - bRadius) <= centerDistance && centerDistance <= aRadius + bRadius)) {
-            //Return a coordinates object with NaN,NaN
-            return new Coordinates2D(Double.NaN, Double.NaN, 0,0);
+
+        if(optCenterDistance.isPresent()) {
+            double centerDistance = optCenterDistance.get();
+            if(!(Math.abs(aRadius - bRadius) <= centerDistance && centerDistance <= aRadius + bRadius)) {
+                //Return a coordinates object with NaN,NaN
+                return Optional.empty();
+            }
+
+            //Set up some variables we need for the calculation
+            double r2 = Math.pow(centerDistance, 2);
+            double a = (Math.pow(aRadius, 2) - Math.pow(bRadius, 2)) / (2 * r2);
+            //aRadius^2 - bRadius^2. Analogous to r2r2 from gist code
+            double aR2bR2 = Math.pow(aRadius, 2) - Math.pow(bRadius, 2);
+            double c = Math.sqrt((2 * Math.pow(aRadius, 2) + Math.pow(bRadius, 2)) / r2 - aR2bR2 / Math.pow(r2, 2) - 1);
+
+            double fx = (this.x + b.getX()) / 2 + a * (b.getX() - this.x);
+            double gx = c * (b.getY() - this.y) / 2;
+            double ix1 = fx + gx;
+            double ix2 = fx - gx;
+
+            double fy = (this.y + b.getY()) / 2 + a * (b.getY() - this.y);
+            double gy = c * (this.x - b.getX()) / 2;
+            double iy1 = fy + gy;
+            double iy2 = fy - gy;
+
+            //The two intersection points are now (ix1, iy1) and (ix2, iy2). We calculate the midpoint of them.
+            double vX = (ix1 + ix2) / 2;
+            double vY = (iy1 + iy2) / 2;
+
+            return Optional.of(new Coordinates2D(vX, vY, 0,0));
+
+        } else {
+            return Optional.empty();
         }
 
-        //Set up some variables we need for the calculation
-        double r2 = Math.pow(centerDistance, 2);
-        double a = (Math.pow(aRadius, 2) - Math.pow(bRadius, 2)) / (2 * r2);
-        //aRadius^2 - bRadius^2. Analogous to r2r2 from gist code
-        double aR2bR2 = Math.pow(aRadius, 2) - Math.pow(bRadius, 2);
-        double c = Math.sqrt((2 * Math.pow(aRadius, 2) + Math.pow(bRadius, 2)) / r2 - aR2bR2 / Math.pow(r2, 2) - 1);
-
-        double fx = (this.x + b.getX()) / 2 + a * (b.getX() - this.x);
-        double gx = c * (b.getY() - this.y) / 2;
-        double ix1 = fx + gx;
-        double ix2 = fx - gx;
-
-        double fy = (this.y + b.getY()) / 2 + a * (b.getY() - this.y);
-        double gy = c * (this.x - b.getX()) / 2;
-        double iy1 = fy + gy;
-        double iy2 = fy - gy;
-
-        //The two intersection points are now (ix1, iy1) and (ix2, iy2). We calculate the midpoint of them.
-        double vX = (ix1 + ix2) / 2;
-        double vY = (iy1 + iy2) / 2;
-
-        //I don't know what the errors should be on the new coordinate so we set them to 0 (no error)
-        return new Coordinates2D(vX, vY, 0,0);
 
     }
 
@@ -93,9 +107,26 @@ public class Coordinates2D {
      * Get a string describing the coordinate.
      * @return A string describing the coordinate
      */
+    @Override
     public String toString() {
-        return String.format("Coordinate centered on (%d, %d) with xErr %d and yErr %d", x, y, xErr, yErr);
+        return String.format("Coordinate centered on (%.4f, %.4f) with xErr %.4f and yErr %.4f", x, y, xErr, yErr);
     }
+
+    @Override
+    public boolean equals(Object b) {
+        if(b == null) {
+            return false;
+        }
+
+        if(b.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Coordinates2D other = (Coordinates2D)b;
+        return other.getX() == this.x && other.getY() == this.y && other.getXErr() == this.xErr && other.getYErr() == this.yErr;
+    }
+
+
 
 
     /*
